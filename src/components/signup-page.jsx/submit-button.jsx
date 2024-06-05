e5b160986557211171cf9b59563070547fdab684
Ali-013 <pgithub0@gmail.com>
2024-06-05 23:57:27 +0500

    Implemented the fonfiguration to import through absolute paths instead of relative paths ---> now use src as main entry for imports

--- src/components/signup-page.jsx/submit-button.jsx
+++ src/components/signup-page.jsx/submit-button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { getButtonText } from "src/utils/basicUtils";
+function SubmitButton({ currentScreen }) {
 
-function SubmitButton(){
-
+    const text = getButtonText(currentScreen)
 
     return(
         <div className='submit-div'>
-            <input type='submit' value='Create Account' className='submit-button' />
+            <input type='submit' value={text} className='submit-button' />
             <div className='prompt-to-signin'>Already have an account? <a href='https://localhost:3000/signin'>Sign in</a></div>
         </div>
     )
