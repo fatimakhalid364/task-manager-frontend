29b1041eccc4f5bfe8748357177f73ec8b5e70bc
Ali-013 <pgithub0@gmail.com>
2024-06-06 19:50:12 +0500

    added the browser router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import App from './App.jsx';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter> {/* Wrap your app with BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
