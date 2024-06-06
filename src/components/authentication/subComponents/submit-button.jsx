97b3680b1c217b5bfab320477f6d0dc007cc725d
Ali-013 <pgithub0@gmail.com>
2024-06-06 20:09:12 +0500

    dynamic url for signin and signup

--- src/components/authentication/subComponents/submit-button.jsx
+++ src/components/authentication/subComponents/submit-button.jsx
@@ -1,12 +1,17 @@
+import PropTypes from 'prop-types';
+import { baseURL, Screen } from "src/constants/constants";
 import { getButtonText, getRoutingStatement, getRoutingText } from "src/utils/basicUtils";
-function SubmitButton({ currentScreen }) {
 
+function SubmitButton({ currentScreen }) {
+    const page = currentScreen === Screen.SIGNIN ? 'signup' : 'signin';
     return(
         <div className='submit-div'>
             <input type='submit' value={getButtonText(currentScreen)} className='submit-button' />
-            <div className='prompt-to-signin'>{getRoutingStatement(currentScreen)}<a href='https://localhost:5173/authentication/signin'>{getRoutingText(currentScreen)}</a></div>
+            <div className='prompt-to-signin'>{getRoutingStatement(currentScreen)}<a href={`${baseURL}/authentication/${page}`}>{getRoutingText(currentScreen)}</a></div>
         </div>
     )
 }
-
+SubmitButton.propTypes = {
+    currentScreen: PropTypes.oneOf([Screen.SIGNUP, Screen.SIGNIN, Screen.FORGOT_PASS, Screen.SET_PASS]),
+};
 export default SubmitButton;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,6 @@
+const localURL = 'http://localhost:5173';
+// const liveURL = 'not yet defined';
+export const baseURL = localURL;
 export const Screen = {
     SIGNUP: 'SIGNUP',
     SIGNIN: 'SIGNIN',
