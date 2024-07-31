50a7a4218e04d280214c45d7e02e8f813c677d76
Ali-013 <pgithub0@gmail.com>
2024-07-31 20:37:43 +0500

    uth issue

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,6 @@ import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { thunk } from 'redux-thunk';
 import { authReducer } from "./slices/authSlice";
-import { searchReducer } from 'src/store/slices/searchSlice';
-import { createTaskReducer } from 'src/store/slices/create_task_slice';
-import { getAllTasksReducer } from 'src/store/slices/get_all_tasks_slice';
 
 
 
@@ -16,15 +13,13 @@ const persistConfig = {
 };
 
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
-const persistCreateTaskReducer = persistReducer(persistConfig, createTaskReducer);
-const persistGetAllTasksReducer = persistReducer(persistConfig, getAllTasksReducer);
+// const persistedUserReducer = persistReducer(persistConfig, authReducer);
+// const persistCreateTaskReducer = persistReducer(persistConfig, createTaskReducer);
+// const persistGetAllTasksReducer = persistReducer(persistConfig, getAllTasksReducer);
 
 export const store = configureStore({
     reducer: {
         auth: persistedAuthReducer,
-        search: searchReducer,
-        createTask: persistCreateTaskReducer,
-        getAllTasks: persistGetAllTasksReducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
@@ -37,3 +32,4 @@ export const persistor = persistStore(store);
 export * from './thunks/authThunks';
 
 export * from 'src/store/thunks/create_task_thunk';
+
