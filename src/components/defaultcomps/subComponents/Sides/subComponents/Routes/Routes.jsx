a964dc11c6cd13b9c17e9d2741fd3df973ab0e61
fatimakhalid364 <khalidfatima364@gmail.com>
2024-08-12 14:10:30 +0500

    fixed the var issue

--- src/components/defaultcomps/subComponents/Sides/subComponents/Routes/Routes.jsx
+++ src/components/defaultcomps/subComponents/Sides/subComponents/Routes/Routes.jsx
@@ -11,11 +11,11 @@ import TaskIcon from "../../../../../icons/TaskIcon";
 function Routes({ clickfunction, burgerMenuClicked }) {
     return (
         <div className='tasks-page-routes-div' >
-            <RoutesComponents icon={DashboardIcon} currentPage='DASHBOARD' route='/dashboard' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
-            <RoutesComponents icon={TaskIcon} currentPage='TASKS' route='/tasks' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
-            <RoutesComponents icon={NotesIcon} currentPage='NOTES' route='/notes' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
-            <RoutesComponents icon={CalendarIcon} currentPage='CALENDER' route='/calendar' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
-            <RoutesComponents icon={GearIcon} currentPage='SETTINGS' route='/settings' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
+            <RoutesComponents icon={DashboardIcon} currentPage='DASHBOARD' page='Dashboard' route='/dashboard' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
+            <RoutesComponents icon={TaskIcon} currentPage='TASKS' page='Tasks' route='/tasks' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
+            <RoutesComponents icon={NotesIcon} currentPage='NOTES' page='Notes' route='/notes' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
+            <RoutesComponents icon={CalendarIcon} currentPage='CALENDER' page='Calendar' route='/calendar' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
+            <RoutesComponents icon={GearIcon} currentPage='SETTINGS' page='Settings' route='/settings' clickfunction={clickfunction} burgerMenuClicked={burgerMenuClicked} />
             <Outlet />
 
         </div>
